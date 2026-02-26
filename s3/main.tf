9c87211089040efb584bab6185ce81e6e65e2ada
karthikvch <karthik998.ch@gmail.com>
2026-02-26 23:23:49 +0530

    changes v3

--- s3/main.tf
+++ s3/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3" {
-  source        = "git::https://github.com/karthikvch/terraform-modules.git//s3?ref=v1.1.1"
+  source        = "git::https://github.com/karthikvch/terraform-modules.git//s3?ref=v1.1.2"
   bucket_name   = var.bucket_name
   versioning    = var.versioning
   force_destroy = var.force_destroy
